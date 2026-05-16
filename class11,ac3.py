c7c836aae0e8f105fce6cd6cc09dee6e8b7b0642
laxmiteja-12345 <laxmitejaadlapalli959@gmail.com>
2026-05-16 11:01:59 +0530

    Add files via upload

---
+++ class11,ac3.py
@@ -0,0 +1,12 @@
+num=int(input("Enter a number: "))
+n=len(str(num))
+sum=0
+temp=num
+while temp>0:
+    digit=temp%10
+    sum=sum+(digit**n)
+    temp //=10
+if num==sum:
+    print(num ,"It is an armstrong number")
+else:
+    print(num ,"is not an armstrong number")
